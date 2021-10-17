a2d79c467a130e526fa5fc7778302ecae3217aa9
Dylnnn-work <@schndylangonzalez@gmail.com>
2021-10-17 07:41:01 -0300

    first commit

--- components/images.jsx
+++ components/images.jsx
@@ -5,7 +5,7 @@ const Imagenes = () => {
         <div Style="display:flex; flex-direction:column; align-self:center;margin-top:0px">
             {fotos.map((image,index) => {
                 return (
-                    <img key={index} src={"/" + image} loader="lazy"  Style="max-width:68vh; max-height:150vh; object-fit:fill;margin-top:0" />
+                    <img key={index} className={styles.responsive} src={"/" + image} loader="lazy"  Style="max-width:130vh; height:auto; object-fit:fill;margin-top:0" />
                     )
             })}
         </div>
